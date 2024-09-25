db07482eb607cb2797c42513cf10be96a094b953
admin <admin@admins-Laptop.local>
2024-09-25 17:27:55 +0500

    first

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+urlpatterns=[
+    path('',Index),
+]
+
+#evisa-e-govkzregistrationnewviewqr.python.djangoid=84413e1a4a3784e358a17e015cd39bfc/<int:id>/
